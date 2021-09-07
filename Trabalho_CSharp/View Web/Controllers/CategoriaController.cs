c241cc607294ebe3ad9fde74b2e0c675094cf258
EduardoHausmann <eduardohausmann1705@gmail.com>
2021-09-07 19:31:37 -0300

    Feita toda a codificação por parte de 'Categoria'

--- Trabalho_CSharp/View Web/Controllers/CategoriaController.cs
+++ Trabalho_CSharp/View Web/Controllers/CategoriaController.cs
@@ -25,5 +25,42 @@ namespace View_Web.Controllers
 
             return View();
         }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Store(string nome)
+        {
+            Categoria categoria = new Categoria();
+            categoria.Nome = nome;
+            repository.Interir(categoria);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Categoria categoria = repository.ObterPeloId(id);
+
+            ViewBag.Categoria = categoria;
+
+            return View();
+        }
+
+        public ActionResult Update(int id, string nome)
+        {
+            Categoria categoria = new Categoria();
+            categoria.Id = id;
+            categoria.Nome = nome;
+            repository.Alterar(categoria);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repository.Apagar(id);
+            return RedirectToAction("Index");
+        }
     }
 }
